93f3162134ae58415d24e916f60679a23e76db52
RaymonIdle <Variluoxx@gmail.com>
2024-07-18 21:57:48 -0700

    add custom items (Ruby & Raw Ruby) + custom ingredient tabs

--- src/main/java/net/raymond/tutorialmod/TutorialMod.java
+++ src/main/java/net/raymond/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.raymond.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.raymond.tutorialmod.item.ModItemGroups;
+import net.raymond.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
